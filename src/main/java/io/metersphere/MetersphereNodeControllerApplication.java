2f17dc60c403894360aaf2efa38a31e57ff04e15
shiziyuan9527 <yuhao.li@fit2cloud.com>
2020-03-11 20:55:52 +0800

    init

---
+++ src/main/java/io/metersphere/MetersphereNodeControllerApplication.java
@@ -0,0 +1,13 @@
+package io.metersphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetersphereNodeControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetersphereNodeControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/metersphere/MetersphereNodeControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.metersphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetersphereNodeControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
