c336f2253cb4d2dca7fe6cb2d0d2a0457c2b6cd6
Captain.B <bin@fit2cloud.com>
2020-03-12 17:20:27 +0800

    test

--- src/main/java/io/metersphere/Application.java
+++ src/main/java/io/metersphere/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetersphereNodeControllerApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetersphereNodeControllerApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ src/test/java/io/metersphere/ApplicationTests.java
@@ -0,0 +1,68 @@
+package io.metersphere;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.*;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+//@SpringBootTest
+class ApplicationTests {
+
+    private static DockerClient dockerClient;
+
+    @BeforeAll
+    static void before() {
+        String usrHome = System.getProperty("user.home");
+
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerConfig(usrHome + File.separator + ".docker")
+                .build();
+        dockerClient = DockerClientBuilder.getInstance(config).build();
+    }
+
+    @Test
+    void createContainer() {
+
+        CreateContainerResponse container
+                = dockerClient.createContainerCmd("mongo:3.6")
+                .withCmd("--bind_ip_all")
+                .withName("mongo")
+                .withHostName("baeldung")
+                .withEnv("MONGO_LATEST_VERSION=3.6")
+                .withPortBindings(PortBinding.parse("9999:27017"))
+                .withBinds(Bind.parse("/tmp/db:/data/db"))
+                .exec();
+    }
+
+    @Test
+    public void testListContainers() {
+        List<Container> containers = dockerClient.listContainersCmd()
+//                .withShowSize(true)
+//                .withShowAll(true)
+//                .withStatusFilter(Arrays.asList("exited"))
+                .exec();
+        containers.forEach(container -> {
+            System.out.println(container.getNames());
+        });
+    }
+
+    @Test
+    public void testImages() {
+        List<Image> images = dockerClient.listImagesCmd()
+                .withShowAll(true)
+                .exec();
+        images.forEach(image -> {
+            System.out.println(image);
+        });
+    }
+
+}
