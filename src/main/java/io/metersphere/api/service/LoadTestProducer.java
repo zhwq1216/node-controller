9a73386037ac8a2ff9ad1414b962a4755ddc1937
Captain.B <bin@fit2cloud.com>
2021-07-13 16:58:13 +0800

    chore: 修改 check kafka 的方法

--- src/main/java/io/metersphere/api/service/LoadTestProducer.java
+++ src/main/java/io/metersphere/api/service/LoadTestProducer.java
@@ -10,7 +10,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -26,16 +25,11 @@ public class LoadTestProducer {
                 //1,建立tcp
                 String ip = ipAndPort[0];
                 int port = Integer.parseInt(ipAndPort[1]);
-                Socket soc = new Socket();
-                // 1s timeout
-                soc.connect(new InetSocketAddress(ip, port), 1000);
-                //2.输入内容
-                String content = "1010";
-                byte[] bs = content.getBytes();
-                OutputStream os = soc.getOutputStream();
-                os.write(bs);
-                //3.关闭
-                soc.close();
+                try (
+                        Socket soc = new Socket()
+                ) {
+                    soc.connect(new InetSocketAddress(ip, port), 1000);
+                }
             }
         } catch (Exception e) {
             LogUtil.error(e);
