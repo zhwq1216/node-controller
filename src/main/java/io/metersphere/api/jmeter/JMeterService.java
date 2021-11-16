a635893c879efd94a720744cc358d5f7fe044fa6
song-tianyang <tianyang.song@fit2cloud.com>
2021-11-16 15:37:30 +0800

    fix(测试计划接口执行): 修复测试计划接口执行时testID赋值错误的问题

--- src/main/java/io/metersphere/api/jmeter/JMeterService.java
+++ src/main/java/io/metersphere/api/jmeter/JMeterService.java
@@ -1,6 +1,7 @@
 package io.metersphere.api.jmeter;
 
 import io.metersphere.api.controller.request.RunRequest;
+import io.metersphere.api.jmeter.constants.ApiRunMode;
 import io.metersphere.api.jmeter.utils.JmeterProperties;
 import io.metersphere.api.jmeter.utils.MSException;
 import io.metersphere.node.util.LogUtil;
@@ -68,7 +69,7 @@ public class JMeterService {
         if (request.getConfig() != null && request.getConfig().getMode().equals("serial") && request.getConfig().getReportType().equals("setReport")) {
             arguments.addArgument(APIBackendListenerClient.TEST_REPORT_ID, request.getConfig().getReportName());
         }
-        if (StringUtils.isNotEmpty(request.getReportId())) {
+        if (StringUtils.isNotEmpty(request.getReportId()) && ApiRunMode.API_PLAN.name().equals(request.getRunMode())) {
             arguments.addArgument(APIBackendListenerClient.TEST_ID, request.getReportId());
         } else {
             arguments.addArgument(APIBackendListenerClient.TEST_ID, request.getTestId());
