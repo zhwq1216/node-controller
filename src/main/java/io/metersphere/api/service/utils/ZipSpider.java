dad66f42d0e66d4f150051e49af95a8f5dbec349
fit2-zhao <yong.zhao@fit2cloud.com>
2021-12-06 10:33:35 +0800

    fix (接口测试): 修复证书问题导致选择资源池执行测试时下载脚本失败问题
    --bug=1008571 --user=赵勇 证书问题导致选择资源池执行测试时下载脚本失败 https://www.tapd.cn/55049933/s/1077829

--- src/main/java/io/metersphere/api/service/utils/ZipSpider.java
+++ src/main/java/io/metersphere/api/service/utils/ZipSpider.java
@@ -6,80 +6,60 @@ import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
 import org.apache.jmeter.protocol.http.util.HTTPFileArg;
 import org.apache.jorphan.collections.HashTree;
 
+import javax.net.ssl.*;
 import java.io.*;
 import java.net.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ZipSpider {
 
-    //根据网址返回网页源代码
-    public static String getHtmlFromUrl(String url, String encoding) {
-        StringBuffer html = new StringBuffer();
-        InputStreamReader isr = null;
-        BufferedReader buf = null;
-        String str = null;
-        try {
-            URL urlObj = new URL(url);
-            URLConnection con = urlObj.openConnection();
-            isr = new InputStreamReader(con.getInputStream(), encoding);
-            buf = new BufferedReader(isr);
-            while ((str = buf.readLine()) != null) {
-                html.append(str + "\n");
-            }
-            //sop(html.toString());
-        } catch (Exception e) {
-            LogUtil.error(e);
-        } finally {
-            if (isr != null) {
-                try {
-                    buf.close();
-                    isr.close();
-                } catch (IOException e) {
-                    LogUtil.error(e);
-
-                }
-            }
+    /**
+     * 覆盖java默认的证书验证
+     */
+    private static final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return new java.security.cert.X509Certificate[]{};
         }
-        return html.toString();
-    }
 
-    //根据网址下载网络文件到硬盘，包括图片，Gif图，以及压缩包
-    public static void download(String url, String path) {
-        File file = null;
-        FileOutputStream fos = null;
-        String downloadName = url.substring(url.lastIndexOf("/") + 1);
-        HttpURLConnection httpCon = null;
-        URLConnection con = null;
-        URL urlObj = null;
-        InputStream in = null;
-        byte[] size = new byte[1024];
-        int num = 0;
-        try {
-            file = new File(path + downloadName);
-            fos = new FileOutputStream(file);
-            if (url.startsWith("http")) {
-                urlObj = new URL(url);
-                con = urlObj.openConnection();
-                httpCon = (HttpURLConnection) con;
-                in = httpCon.getInputStream();
-                while ((num = in.read(size)) != -1) {
-                    for (int i = 0; i < num; i++)
-                        fos.write(size[i]);
-                }
-            }
-
-        } catch (Exception e) {
-            LogUtil.error(e);
-        } finally {
-            try {
-                in.close();
-                fos.close();
-            } catch (Exception e) {
-                LogUtil.error(e);
-            }
+        public void checkClientTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
         }
+
+        public void checkServerTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+        }
+    }};
+
+    /**
+     * 设置不验证主机
+     */
+    private static final HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
+
+    /**
+     * 信任所有
+     *
+     * @param connection
+     * @return
+     */
+    private static SSLSocketFactory trustAllHosts(HttpsURLConnection connection) {
+        SSLSocketFactory oldFactory = connection.getSSLSocketFactory();
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            SSLSocketFactory newFactory = sc.getSocketFactory();
+            connection.setSSLSocketFactory(newFactory);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return oldFactory;
     }
 
     //解压本地文件至目的文件路径
@@ -108,34 +88,6 @@ public class ZipSpider {
         } catch (IOException e) {
             LogUtil.error(e);
         }
-    }
-
-    //从总目录下解压文件里所有的压缩包至目的文件路径
-    public static void unzipFromLoc(String filePath) throws Exception {
-        File file = new File(filePath);
-        File[] list = file.listFiles();
-        String from = "";
-        String to = "E:\\myDownload\\unzipFileFromWeb\\";
-        for (File f : list) {
-            boolean bool = f.isFile();
-            if (bool) {
-                from = f.getAbsolutePath();
-                from = from.replace("\\", "\\\\");
-                sop(from);
-                unzip(from, to);
-            }
-        }
-    }
-
-    public static void sop(Object obj) {
-        LogUtil.info(obj);
-    }
-
-    public static void seperate(char c) {
-        for (int x = 0; x < 100; x++) {
-            System.out.print(c);
-        }
-        sop("");
     }
 
     public static void getFiles(HashTree tree, List<BodyFile> files) {
@@ -174,6 +126,12 @@ public class ZipSpider {
         try {
             URL url = new URL(urlPath);
             URLConnection urlConnection = url.openConnection();
+            boolean useHttps = urlPath.startsWith("https");
+            if (useHttps) {
+                HttpsURLConnection https = (HttpsURLConnection) urlConnection;
+                trustAllHosts(https);
+                https.setHostnameVerifier(DO_NOT_VERIFY);
+            }
             httpURLConnection = (HttpURLConnection) urlConnection;// http的连接类
             httpURLConnection.setConnectTimeout(1000 * 5);//设置超时
             httpURLConnection.setRequestMethod("GET");//设置请求方式，默认是GET
@@ -204,13 +162,9 @@ public class ZipSpider {
             LogUtil.info("文件下载成功！");
             return file;
         } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
             LogUtil.error(e);
-
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             LogUtil.error(e);
-
             LogUtil.info("文件下载失败！");
         } finally {
             try {
